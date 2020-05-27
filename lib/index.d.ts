e39b6496a4e8b268357b4e30a3c8b1fb0df2f994
YarielInfante <yarielinfante@gmail.com>
2020-05-27 12:58:25 -0400

    adding initial interfaces

---
+++ lib/index.d.ts
@@ -0,0 +1,16 @@
+export interface Payload {
+    id: string;
+    username: string;
+}
+export interface UserDto {
+    email: string;
+    username: string;
+    firstName: string;
+    lastName: string;
+}
+export interface UserLoginDTO extends UserDto {
+    readonly password: string;
+}
+export interface UserRegisterDTO extends UserDto {
+    password: string;
+}
